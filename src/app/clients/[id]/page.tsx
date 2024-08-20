49486813b776ed5159c77018b4d2bdc5b0158433
VukZh <vukfx@inbox.ru>
2024-08-20 20:03:18 +0500

    chore: install antd

---
+++ src/app/clients/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function ClientID() {
+  return (
+    <div>
+      <h2>Client ID</h2>
+    </div>
+  );
+}
---
+++ src/app/managers/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function ManagerID() {
+  return (
+    <div>
+      <h2>Manager ID</h2>
+    </div>
+  );
+}
